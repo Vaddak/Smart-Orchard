0776da6c55d9d86292d35846cb052c4b923f1775
Valentina Restrepo <valentina.restrepoj@udea.edu.co>
2024-06-14 11:55:35 -0500

    Update KY-018.c

--- lib/light-intensity/KY-018.c
+++ lib/light-intensity/KY-018.c
@@ -7,32 +7,31 @@
 
 #define NO_OF_SAMPLES   64          // Número de muestras para promediar
 
-static const char *TAG = "Light Sensor";
-static adc_oneshot_unit_handle_t adc1_handle;
+static adc_oneshot_unit_handle_t adc2_handle; // Cambio de adc1_handle a adc2_handle
 static adc_cali_handle_t cali_handle;
 
 void init_light_sensor() {
-   // Inicializar el ADC
+   // Inicializar el ADC 2
    adc_oneshot_unit_init_cfg_t init_config = {
-       .unit_id = ADC_UNIT_1,
+       .unit_id = ADC_UNIT_2, // Cambio a ADC_UNIT_2
    };
-   adc_oneshot_new_unit(&init_config, &adc1_handle);
+   adc_oneshot_new_unit(&init_config, &adc2_handle); // Cambio a adc2_handle
 
    adc_oneshot_chan_cfg_t config = {
        .bitwidth = ADC_BITWIDTH_DEFAULT,
        .atten = ADC_ATTEN_DB_0,
    };
-   adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &config); // Usando GPIO39
+   adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL_9, &config); // Cambio a ADC_CHANNEL_9 en GPIO 26
 
    // Calibración del ADC
    adc_cali_line_fitting_config_t cali_config = {
-       .unit_id = ADC_UNIT_1,
+       .unit_id = ADC_UNIT_2, // Cambio a ADC_UNIT_2
        .atten = ADC_ATTEN_DB_0,
        .bitwidth = ADC_BITWIDTH_DEFAULT,
    };
    esp_err_t cali_ret = adc_cali_create_scheme_line_fitting(&cali_config, &cali_handle);
    if (cali_ret != ESP_OK) {
-       ESP_LOGE(TAG, "Calibración del ADC fallida");
+     //  ESP_LOGE(TAG, "Calibración del ADC fallida");
        cali_handle = NULL;
    }
 }
@@ -42,7 +41,7 @@ uint32_t read_light_intensity() {
    // Multisampling
    for (int i = 0; i < NO_OF_SAMPLES; i++) {
        int raw;
-       adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &raw); // Usando GPIO39
+       adc_oneshot_read(adc2_handle, ADC_CHANNEL_9, &raw); // Cambio a adc2_handle y ADC_CHANNEL_9
        adc_reading += raw;
    }
    adc_reading /= NO_OF_SAMPLES;
@@ -54,7 +53,7 @@ int read_light_voltage() {
    // Multisampling
    for (int i = 0; i < NO_OF_SAMPLES; i++) {
        int raw;
-       adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &raw); // Usando GPIO39
+       adc_oneshot_read(adc2_handle, ADC_CHANNEL_9, &raw); // Cambio a adc2_handle y ADC_CHANNEL_9
        adc_reading += raw;
    }
    adc_reading /= NO_OF_SAMPLES;
@@ -64,7 +63,7 @@ int read_light_voltage() {
    if (cali_handle != NULL) {
        adc_cali_raw_to_voltage(cali_handle, adc_reading, &voltage);
    } else {
-       ESP_LOGW(TAG, "Calibración no disponible, usando lectura sin calibrar");
+      // ESP_LOGW(TAG, "Calibración no disponible, usando lectura sin calibrar");
        voltage = adc_reading * 3300 / 4095; // Aproximación del voltaje sin calibración
    }
    return voltage;
